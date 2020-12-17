3d8ac9dc768bf5741efb215b10802142f2e99bb0
Chinawat <memorytao@gmail.com>
2020-12-17 10:55:51 +0700

    change name Access to Login

--- src/main/java/com/login/LoginController.java
+++ src/main/java/com/login/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(allowedHeaders = "*", origins = "*")
 @RestController
-public class AccessController extends Exception {
+public class LoginController extends Exception {
 
 	/**
 	 * 
--- src/main/java/com/login/LoginError.java
+++ src/main/java/com/login/LoginError.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Actor Not Found")
-public class AccessError extends Exception{
+public class LoginError extends Exception{
 
 	/**
 	 * 
